cbe6785f1324298f83913dd7008abc08c0293c9d
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-29 20:22:54 -0300

    feat: ensure app receive routes dinamically

--- src/configExpress/app.ts
+++ src/configExpress/app.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 import setUpMiddlewares from './middlewares'
-
+import setUpRoutes from './routes'
 const app = express()
 setUpMiddlewares(app)
-
+setUpRoutes(app)
 export default app
