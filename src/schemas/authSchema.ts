6ca193c6676515ae46d4f9203ef29ee29bbf0af7
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-30 20:28:58 -0300

    refactor: add name to schema

--- src/schemas/authSchema.ts
+++ src/schemas/authSchema.ts
@@ -1,6 +1,7 @@
 import Joi from 'joi'
 
 export const signUpSchema = Joi.object({
+  name: Joi.string().required().error(new Error('Você deve fornecer um nome válido!')),
   email: Joi.string().email().required().error(new Error('Você deve fornecer um e-mail válido!')),
   password: Joi.string().required().error(new Error('Uma senha deve ser fornecida!')),
   passwordConfirmation: Joi.string().required().valid(Joi.ref('password')).error(new Error('A confirmação da senha deve ser igual!'))
