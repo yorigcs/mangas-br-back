595d7a2803d6047b8224279e740db58223677f05
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-30 20:20:15 -0300

    feat: implements createUserService to controller

--- src/controllers/signUpController.ts
+++ src/controllers/signUpController.ts
@@ -1,5 +1,13 @@
 import { Request, Response } from 'express'
+import { createUserService } from '../services/usersService'
 
-export const signUpController = async (req: Request, res: Response): Promise<void> => {
-  res.send('ok')
+import { InserUserData } from '../models/userModels'
+interface RequestUser extends Request {
+  body: InserUserData
+}
+
+export const signUpController = async (req: RequestUser, res: Response): Promise<void> => {
+  const userData = req.body
+  const user = await createUserService(userData)
+  res.status(201).send(user)
 }
