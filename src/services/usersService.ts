7222b413627ca796e59224910b39f5077477cdbb
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-30 20:29:44 -0300

    feat: ensure if an user does not exists create user

--- src/services/usersService.ts
+++ src/services/usersService.ts
@@ -1,9 +1,10 @@
 import { ConflictError } from '../errors/conflictError'
-import { findUserByEmail } from '../repositories/usersRepository'
-import { InserUserData } from '../models/userModels'
-export const createUserService = async (data: InserUserData): Promise<any> => {
+import { createUser, findUserByEmail } from '../repositories/usersRepository'
+import { InsertUserData } from '../models/userModels'
+
+export const createUserService = async (data: InsertUserData): Promise<any> => {
   const user = await findUserByEmail(data.email)
-  if (user) {
-    throw new ConflictError('Este usuário já está cadastrado!')
-  }
+  if (user) throw new ConflictError('Este usuário já está cadastrado!')
+
+  return await createUser(data)
 }
