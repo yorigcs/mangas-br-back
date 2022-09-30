f1df8caef3a7368137003d3b545af7efb361c4e3
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-30 20:50:39 -0300

    feat: ensure password is encrypted and returns only id,name and email

--- src/services/usersService.ts
+++ src/services/usersService.ts
@@ -1,10 +1,22 @@
 import { ConflictError } from '../errors/conflictError'
 import { createUser, findUserByEmail } from '../repositories/usersRepository'
 import { InsertUserData } from '../models/userModels'
+import { hash } from '../utils/bcryptAdapter'
 
 export const createUserService = async (data: InsertUserData): Promise<any> => {
   const user = await findUserByEmail(data.email)
   if (user) throw new ConflictError('Este usuário já está cadastrado!')
 
-  return await createUser(data)
+  const salt = 12
+  const userCreated = await createUser(Object.assign({}, data, { password: hash(data.password, salt) }))
+  const {
+    password,
+    profile_picture: profilePicture,
+    is_admin: isAdmin,
+    is_moderator: isModerator,
+    created_at: createdAt,
+    updated_at: updatedAt
+    , ...userData
+  } = userCreated
+  return userData
 }
