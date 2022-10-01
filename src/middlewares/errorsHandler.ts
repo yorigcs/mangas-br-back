8ac8deb39a51a7e9a774c0b0cad2fba096bde288
Yorí Galisteu <yorigcsdev@gmail.com>
2022-09-30 21:28:48 -0300

    feat: add instanceof UnauthorizedError condition

--- src/middlewares/errorsHandler.ts
+++ src/middlewares/errorsHandler.ts
@@ -1,7 +1,9 @@
 import { Request, Response, NextFunction } from 'express'
 import { ConflictError } from '../errors/conflictError'
+import { UnauthorizedError } from '../errors/unauthorizedError'
 
 export const errorsHandler = (err: Error, _req: Request, res: Response, next: NextFunction): Response<any, Record<string, any>> => {
   if (err instanceof ConflictError) return res.status(409).json({ error: err.message })
+  else if (err instanceof UnauthorizedError) return res.status(401).json({ error: err.message })
   next(err)
 }
