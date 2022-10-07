1a512d7bb3af0ca6ed7576dd79ed87f1f47c981c
Yorí Galisteu <yorigcsdev@gmail.com>
2022-10-07 00:13:39 -0300

    feat: ensure user is admin to continue

--- src/services/mangas/mangaCreateService.ts
+++ src/services/mangas/mangaCreateService.ts
@@ -1,8 +1,13 @@
 import { ConflictError } from '../../errors/conflictError'
 import { findMangaByName, createManga } from '../../repositories/mangasRepository'
 import { MangaData } from '../../models/mangaModels'
+import { findUserById } from '../../repositories/usersRepository'
+import { ForbiddenError } from '../../errors/forbiddenError'
 
-export const createMangaService = async (data: MangaData): Promise<any> => {
+export const createMangaService = async (data: MangaData, userId: string): Promise<any> => {
+  const user = await findUserById(userId)
+  if (!user?.is_admin) throw new ForbiddenError('Você não tem permissão para acessar essa rota!')
+
   const manga = await findMangaByName(data.name)
   if (manga) throw new ConflictError('Este manga já está cadastrado!')
 
