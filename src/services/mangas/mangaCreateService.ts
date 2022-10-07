589e6ad67d4ebe5e77763d2450227fff70716a71
Yorí Galisteu <yorigcsdev@gmail.com>
2022-10-06 22:39:54 -0300

    feat: create new manga if there is no conflict

--- src/services/mangas/mangaCreateService.ts
+++ src/services/mangas/mangaCreateService.ts
@@ -1,9 +1,13 @@
 import { ConflictError } from '../../errors/conflictError'
-import { findMangaByName } from '../../repositories/mangasRepository'
-import { InsertMangaData } from '../../models/mangaModels'
+import { findMangaByName, createManga } from '../../repositories/mangasRepository'
+import { MangaData } from '../../models/mangaModels'
 
-export const createMangaService = async (data: InsertMangaData): Promise<any> => {
+export const createMangaService = async (data: MangaData): Promise<any> => {
   const manga = await findMangaByName(data.name)
   if (manga) throw new ConflictError('Este manga já está cadastrado!')
-  return 'ok'
+  const { genres, ...insertMangaData } = data
+
+  const mangaCreation = await createManga(insertMangaData)
+
+  return mangaCreation
 }
