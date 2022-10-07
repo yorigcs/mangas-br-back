9472971e6ed70e0f8498d1633d14b9a2d58640bd
Yorí Galisteu <yorigcsdev@gmail.com>
2022-10-07 14:24:31 -0300

    feat: add createGenreManga

--- src/repositories/mangasGenresRepository.ts
+++ src/repositories/mangasGenresRepository.ts
@@ -4,3 +4,7 @@ import { GenreManga } from '../models/mangaModels'
 export const findGenreMangaByMangaIdAndGenreId = async (mangaId: string, genreId: string): Promise<GenreManga> => {
   return await prisma.genreManga.findFirst({ where: { AND: [{ genre_id: genreId }, { manga_id: mangaId }] } })
 }
+
+export const createGenreManga = async (mangaId: string, genreId: string): Promise<GenreManga> => {
+  return await prisma.genreManga.create({ data: { manga_id: mangaId, genre_id: genreId } })
+}
