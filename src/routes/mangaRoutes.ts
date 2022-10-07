1cef99b17050650b674bb9374b9e5fbb16fe3899
Yorí Galisteu <yorigcsdev@gmail.com>
2022-10-07 00:18:20 -0300

    refactor: change files name to mangaRoutes

--- src/routes/mangaRoutes.ts
+++ src/routes/mangaRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
 import { mangaCreateController } from '../controllers/mangaCreateController'
+import { authValidation } from '../middlewares/authValidation'
 
 import { bodyValidation } from '../middlewares/bodyValidation'
 import { createMangaSchema } from '../schemas/mangaSchemas'
 
 export default (router: Router): void => {
-  router.post('/create-manga', bodyValidation(createMangaSchema), mangaCreateController)
+  router.post('/create-manga', authValidation, bodyValidation(createMangaSchema), mangaCreateController)
 }
