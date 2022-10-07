4a3ae92023a46b7141ff430cf4a65b07800265d2
Yorí Galisteu <yorigcsdev@gmail.com>
2022-10-07 13:59:03 -0300

    feat: create route add-genre-to-manga

--- src/routes/mangaRoutes.ts
+++ src/routes/mangaRoutes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express'
+import { mangaAddGenreController } from '../controllers/mangaAddGenreController'
 import { mangaCreateController } from '../controllers/mangaCreateController'
 import { authValidation } from '../middlewares/authValidation'
 
 import { bodyValidation } from '../middlewares/bodyValidation'
+import { genreSchema } from '../schemas/genresSchemas'
 import { createMangaSchema } from '../schemas/mangaSchemas'
 
 export default (router: Router): void => {
   router.post('/create-manga', authValidation, bodyValidation(createMangaSchema), mangaCreateController)
+  router.post('/add-genre-to-manga', authValidation, bodyValidation(genreSchema), mangaAddGenreController)
 }
