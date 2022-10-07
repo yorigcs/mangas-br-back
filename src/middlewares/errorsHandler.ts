a46a3f88cae91c9629f42c68c7654e1ca8db90db
Yorí Galisteu <yorigcsdev@gmail.com>
2022-10-07 15:07:32 -0300

    fix: change NotFoundError to correct path

--- src/middlewares/errorsHandler.ts
+++ src/middlewares/errorsHandler.ts
@@ -1,10 +1,11 @@
-import { NotFoundError } from '@prisma/client/runtime'
+import { NotFoundError } from '../errors/notFoundError'
 import { Request, Response, NextFunction } from 'express'
 import { ConflictError } from '../errors/conflictError'
 import { ForbiddenError } from '../errors/forbiddenError'
 import { UnauthorizedError } from '../errors/unauthorizedError'
 
 export const errorsHandler = (err: Error, _req: Request, res: Response, next: NextFunction): Response<any, Record<string, any>> => {
+  console.log(err.message)
   if (err instanceof ConflictError) return res.status(409).json({ error: err.message })
   else if (err instanceof UnauthorizedError) return res.status(401).json({ error: err.message })
   else if (err instanceof ForbiddenError) return res.status(403).json({ error: err.message })
