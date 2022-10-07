46f1deb23bffe043357f4506a25a4676adf54a67
Yorí Galisteu <yorigcsdev@gmail.com>
2022-10-07 00:10:44 -0300

    feat: returns 403 if error is instance of Forbidden

--- src/middlewares/errorsHandler.ts
+++ src/middlewares/errorsHandler.ts
@@ -1,9 +1,11 @@
 import { Request, Response, NextFunction } from 'express'
 import { ConflictError } from '../errors/conflictError'
+import { ForbiddenError } from '../errors/forbiddenError'
 import { UnauthorizedError } from '../errors/unauthorizedError'
 
 export const errorsHandler = (err: Error, _req: Request, res: Response, next: NextFunction): Response<any, Record<string, any>> => {
   if (err instanceof ConflictError) return res.status(409).json({ error: err.message })
   else if (err instanceof UnauthorizedError) return res.status(401).json({ error: err.message })
-  next(err)
+  else if (err instanceof ForbiddenError) return res.status(403).json({ error: err.message })
+  return res.status(500).json({ error: 'Ocorreu um erro interno, tente novamente mais tarde!' })
 }
