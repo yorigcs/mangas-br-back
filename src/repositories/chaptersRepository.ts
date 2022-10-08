e60d272a92b22495318963b7ddbebf2e9c055aea
Yorí Galisteu <yorigcsdev@gmail.com>
2022-10-08 01:03:32 -0300

    feat: create findChapterById

--- src/repositories/chaptersRepository.ts
+++ src/repositories/chaptersRepository.ts
@@ -8,3 +8,7 @@ export const createChapter = async (insertData: ChapterData): Promise<Chapter> =
 export const findChapterBySeasonAndChapterNum = async (season: number, chapterNum: number): Promise<Chapter> => {
   return await prisma.chapter.findFirst({ where: { AND: [{ season }, { chapter_num: chapterNum }] } })
 }
+
+export const findChapterById = async (id: string): Promise<Chapter> => {
+  return await prisma.chapter.findFirst({ where: { id } })
+}
