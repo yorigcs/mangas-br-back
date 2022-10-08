745b53d9dd75513280e91dc05474183fcb5b5ea9
Yorí Galisteu <yorigcsdev@gmail.com>
2022-10-08 01:03:09 -0300

    feat: create pageModels

---
+++ src/models/pageModels.ts
@@ -0,0 +1,22 @@
+export interface Page {
+  id: string
+  name: string
+  page_img: string
+  page_num: number
+  chapter_id: string
+  created_at: Date
+  updated_at: Date
+
+}
+
+export interface InsertPageData {
+  name: string
+  page_img: string
+  page_num: number
+  chapter_id: string
+}
+
+export interface PageData {
+  page_img: string[]
+  chapter_id: string
+}
