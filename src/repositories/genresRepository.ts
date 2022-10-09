a8949bd5902fabaa8a8cb7a954e0e3ee7c20535d
Yorí Galisteu <yorigcsdev@gmail.com>
2022-10-09 00:36:06 -0300

    feat: find all genres availables

--- src/repositories/genresRepository.ts
+++ src/repositories/genresRepository.ts
@@ -4,3 +4,7 @@ import { Genre } from '../models/genreModels'
 export const findGenresByName = async (name: string): Promise<Genre> => {
   return await prisma.genre.findFirst({ where: { name } })
 }
+
+export const findAllGenres = async (): Promise<Genre[]> => {
+  return await prisma.genre.findMany({})
+}
