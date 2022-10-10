daaa3edc60c925f3abf2ef5fac457e0e001905f3
Yorí Galisteu <yorigcsdev@gmail.com>
2022-10-10 16:31:13 -0300

    fix: change cors setHeaders

--- src/configExpress/middlewares/cors.ts
+++ src/configExpress/middlewares/cors.ts
@@ -1,8 +1,9 @@
 import { Request, Response, NextFunction } from 'express'
 
 export const cors = (_req: Request, res: Response, next: NextFunction): void => {
-  res.set('access-control-allow-origin', '*')
-  res.set('access-control-allow-methods', '*')
-  res.set('access-control-allow-headers', '*')
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', '*')
+  res.setHeader('Access-Control-Allow-Headers', '*')
+
   next()
 }
