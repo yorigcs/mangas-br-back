d23346074d69f21e5041301faf4e84ffa57f822c
Yorí Galisteu <yorigcsdev@gmail.com>
2022-10-10 14:54:44 -0300

    chore: add extra configs

--- jest.config.ts
+++ jest.config.ts
@@ -5,37 +5,11 @@ export default {
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   // collectCoverageFrom: undefined,
 
-  // The directory where Jest should output its coverage files
+  roots: ['<rootDir>/tests'],
   coverageDirectory: 'coverage',
-
-  // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
-
-  // Indicates which provider should be used to instrument code for coverage
   coverageProvider: 'v8',
-
-  // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
-  // modulePathIgnorePatterns: [],
-
-  // Automatically reset mock state before every test
-  // resetMocks: false,
-
-  // The glob patterns Jest uses to detect test files
-  // testMatch: [
-  //   "**/__tests__/**/*.[jt]s?(x)",
-  //   "**/?(*.)+(spec|test).[tj]s?(x)"
-  // ],
-
-  // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  // testPathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
-
-  // A map from regular expressions to paths to transformers
+  collectCoverageFrom: ['<rootDir>/src/services/*.ts'],
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
-
 }
