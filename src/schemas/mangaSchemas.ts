ec1476968130b3eee151d390aed5f61bed6d3a7d
Yorí Galisteu <yorigcsdev@gmail.com>
2022-10-20 22:33:09 -0300

    refactor: remove coverPicture

--- src/schemas/mangaSchemas.ts
+++ src/schemas/mangaSchemas.ts
@@ -2,7 +2,6 @@ import Joi from 'joi'
 
 export const createMangaSchema = Joi.object({
   name: Joi.string().required().error(new Error('Você deve fornecer um nome válido!')),
-  coverPicture: Joi.string().pattern(/(http)?s?:?(\/\/[^"']*\.(?:png|jpg|jpeg|gif|png|svg))/).required().error(new Error('Você deve fornecer um link de  imagem válida!')),
   description: Joi.string().required().error(new Error('Você deve fornercer uma descrição!')),
   author: Joi.string().required().error(new Error('Você deve fornercer o nome do autor!'))
 })
