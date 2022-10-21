a67ddeb6dbef2c4f0908968df9230df5f511d736
Yorí Galisteu <yorigcsdev@gmail.com>
2022-10-20 22:28:56 -0300

    refactor: change body to validation

--- src/middlewares/bodyValidation.ts
+++ src/middlewares/bodyValidation.ts
@@ -3,7 +3,8 @@ import { Response, Request, NextFunction } from 'express'
 
 export const bodyValidation = (schema: joi.ObjectSchema) => {
   return (req: Request, res: Response, next: NextFunction) => {
-    const { error } = schema.validate(req.body)
+    const body = JSON.parse(JSON.stringify(req.body))
+    const { error } = schema.validate(body)
     if (error) {
       return res.status(422).json({ error: error.message })
     }
