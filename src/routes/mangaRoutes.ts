8bf3ca4899d10776f8ae5cf55bee6eee604ddc7e
Yorí Galisteu <yorigcsdev@gmail.com>
2022-10-20 22:35:20 -0300

    feat: add multerPload middleware to create-manga route

--- src/routes/mangaRoutes.ts
+++ src/routes/mangaRoutes.ts
@@ -4,11 +4,12 @@ import * as manga from '../controllers/mangaControllers'
 import { authValidation } from '../middlewares/authValidation'
 
 import { bodyValidation } from '../middlewares/bodyValidation'
+import { multerUpload } from '../middlewares/multerUpload'
 import { genreSchema } from '../schemas/genresSchemas'
 import { createMangaSchema } from '../schemas/mangaSchemas'
 
 export default (router: Router): void => {
-  router.post('/create-manga', authValidation, bodyValidation(createMangaSchema), manga.createManga)
+  router.post('/create-manga', authValidation, multerUpload, bodyValidation(createMangaSchema), manga.createManga)
   router.post('/add-genre-to-manga', authValidation, bodyValidation(genreSchema), manga.addGenreToManga)
   router.get('/mangas-with-chapters', manga.findMangasWithChapter)
   router.get('/all-mangas', manga.findMangasWithCategory)
