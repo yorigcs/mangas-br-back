cec17aa1c95aa873598c874b2174b49d5528bc0a
Yorí Galisteu <yorigcsdev@gmail.com>
2022-10-24 19:57:54 -0300

    feat: ensure at least one valid image is provided

--- src/middlewares/multerUpload.ts
+++ src/middlewares/multerUpload.ts
@@ -6,8 +6,13 @@ import { checkDir } from '../utils/checkDir'
 
 const dest = path.join(__dirname, '../public/uploads/temp')
 
+const whiteList = [
+  'image/png',
+  'image/jpeg',
+  'image/jpg'
+]
 const storage = multer.diskStorage({
-  destination: function (_req, file, cb) {
+  destination: function (_req, _file, cb) {
     checkDir(dest)
     cb(null, dest)
   },
@@ -22,12 +27,25 @@ const storage = multer.diskStorage({
   }
 })
 
-const upload = multer({ storage }).array('images', 100)
+const upload = multer(
+  {
+    storage,
+    fileFilter: (req, file, cb) => {
+      if (!whiteList.includes(file.mimetype)) {
+        return cb(new Error('Apenas imagens são permitidas!'))
+      }
+      cb(null, true)
+    }
+  }
+)
+  .array('images', 100)
 
 export const multerUpload = (req: Request, res: Response, next: NextFunction): void => {
   upload(req, res, (err: any): any => {
     if (err instanceof multer.MulterError) {
       return res.status(400).send(err.message)
+    } else if (!req.files) {
+      return res.status(400).send('Você deve enviar pelo menos uma imagem!')
     } else if (err) {
       return res.status(400).send(err.message)
     } else {
