72dd96006c7914fda4dec7dc953b36370b3daa34
Yorí Galisteu <yorigcsdev@gmail.com>
2022-11-28 19:40:29 -0300

    refactor: change multerUpload calls

--- src/routes/mangaRoutes.ts
+++ src/routes/mangaRoutes.ts
@@ -9,7 +9,7 @@ import { genreSchema } from '../schemas/genresSchemas'
 import { createMangaSchema } from '../schemas/mangaSchemas'
 
 export default (router: Router): void => {
-  router.post('/create-manga', authValidation, multerUpload, bodyValidation(createMangaSchema), manga.createManga)
+  router.post('/create-manga', authValidation, multerUpload(), bodyValidation(createMangaSchema), manga.createManga)
   router.post('/add-genre-to-manga', authValidation, bodyValidation(genreSchema), manga.addGenreToManga)
   router.get('/mangas-with-chapters', manga.findMangasWithChapter)
   router.get('/all-mangas', manga.findMangasWithCategory)
--- src/routes/pageRoutes.ts
+++ src/routes/pageRoutes.ts
@@ -3,9 +3,10 @@ import * as page from '../controllers/pageController'
 
 import { authValidation } from '../middlewares/authValidation'
 import { bodyValidation } from '../middlewares/bodyValidation'
+import { multerUpload } from '../middlewares/multerUpload'
 
 import { createPageSchema } from '../schemas/pageSchemas'
 
 export default (router: Router): void => {
-  router.post('/create-page', authValidation, bodyValidation(createPageSchema), page.createPage)
+  router.post('/create-page', authValidation, multerUpload(50), bodyValidation(createPageSchema), page.createPage)
 }
