c6cc6a8c015e5aaff01375c9b01e8c321e19c8be
Yorí Galisteu <yorigcsdev@gmail.com>
2022-11-28 19:45:48 -0300

    refactor: change page creation to save files

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -2,13 +2,14 @@ import { Request, Response } from 'express'
 import * as pageService from '../services/pageService'
 
 import { PageData } from '../models/pageModels'
+import { Files } from '../models/files'
 
 interface RequestUserCreatePage extends Request {
   body: PageData
 }
 
 const createPage = async (req: RequestUserCreatePage, res: Response): Promise<void> => {
-  await pageService.pageCreate(req.body, res.locals.id)
+  await pageService.pageCreate(req.body, res.locals.id, req.files as Files)
   res.status(201).send('Páginas criadas com sucesso!')
 }
 export { createPage }
--- src/services/pageService.ts
+++ src/services/pageService.ts
@@ -1,29 +1,39 @@
 import * as chaptersRepository from '../repositories/chaptersRepository'
 import * as usersRepository from '../repositories/usersRepository'
 import * as pagesRepository from '../repositories/pagesRepository'
+import * as mangasRepository from '../repositories/mangasRepository'
 
 import { PageData } from '../models/pageModels'
+import { Files } from '../models/files'
 
 import { ForbiddenError } from '../errors/forbiddenError'
 import { ConflictError } from '../errors/conflictError'
 import { NotFoundError } from '../errors/notFoundError'
+import { handleSaveFiles } from '../utils/handleSaveFiles'
 
-const pageCreate = async (data: PageData, userId: string): Promise<any> => {
+const pageCreate = async (data: PageData, userId: string, files: Files): Promise<any> => {
   const user = await usersRepository.findUserById(userId)
   if (user?.role !== 'admin') throw new ForbiddenError('Você não tem permissão para acessar essa rota!')
 
   const chapter = await chaptersRepository.findChapterById(data.chapter_id)
   if (!chapter) throw new NotFoundError('Este capítulo não existe!')
 
-  for (let i = 0; i < data.page_img.length; i++) {
-    const page = pagesRepository.findPageByPageNumAndChapterId(i + 1, data.chapter_id)
+  const manga = await mangasRepository.findMangaById(chapter.manga_id)
+
+  for (let i = 0; i < files.length; i++) {
+    const page = await pagesRepository.findPageByPageNumAndChapterId(i + 1, data.chapter_id)
     if (page) throw new ConflictError(`A página ${1 + i} já existe nesse capítulo!`)
+  }
+
+  const urls = await handleSaveFiles(`${manga.name.split(' ').join('-').toLowerCase()}/chapter-${chapter.chapter_num}`, files)
+
+  for (let i = 0; i < urls.length; i++) {
     await pagesRepository.createPage(
       {
         chapter_id: data.chapter_id,
         name: `Página ${i + 1}`,
         page_num: i + 1,
-        page_img: data.page_img[i]
+        page_img: urls[i]
       }
     )
   }
