dc6481743f194aff978240cabe9dc93994b1e770
Yorí Galisteu <yorigcsdev@gmail.com>
2022-12-09 15:30:18 -0300

    feat: create findPagesByChapterId controller

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -8,8 +8,18 @@ interface RequestUserCreatePage extends Request {
   body: PageData
 }
 
+interface RequestFindPagesByChapterId extends Request {
+  params: { chapterId: string }
+}
+
 const createPage = async (req: RequestUserCreatePage, res: Response): Promise<void> => {
   await pageService.pageCreate(req.body, res.locals.id, req.files as Files)
   res.status(201).send('Páginas criadas com sucesso!')
 }
-export { createPage }
+
+const findPagesByChapterId = async (req: RequestFindPagesByChapterId, res: Response): Promise<void> => {
+  const { chapterId } = req.params
+  const pages = await pageService.findPagesByChapterId(chapterId)
+  res.status(200).send(pages)
+}
+export { createPage, findPagesByChapterId }
